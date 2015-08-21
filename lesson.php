00a3cb77abaf8d9618624511b0b65b17bcaf8fb3
pinta <ruslan@pinta.com.ua>
2015-08-21 14:11:14 +0300

    lesson

--- lesson.php
+++ lesson.php
@@ -1,78 +1,39 @@
 <?php
 public function lessonsTargetAction(Request $request)
+{
+    $returns = array('status' => false);
 
-    {
+    if ($request->isMethod('POST') && $request->get('action')) {
+        $em = $this->getDoctrine()->getManager();
 
-        $returns = array('status' => false);
+        $action = $request->get('action');
+        $returns['action'] = $action;
 
+        if ($action == 'add') {
+            $target = new \App\MainBundle\Entity\LessonsTarget();
+        }
+        if (is_numeric($request->get('id')) && $action == 'edit' || $action == 'delete') {
+            $target= $em->getRepository('AppMainBundle:LessonsTarget')->find($request->get('id'));
 
-
-        if ($request->isMethod('POST') && $request->get('action')) {
-
-            $em = $this->getDoctrine()->getManager();
-
-
-
-            $action = $request->get('action');
-
-            $returns['action'] = $action;
-
-
-
-            if ($action == 'add') {
-
-                $target = new \App\MainBundle\Entity\LessonsTarget();
-
-
-
-            }
-
-            if (is_numeric($request->get('id')) && $action == 'edit' || $action == 'delete') {
-
-                $target= $em->getRepository('AppMainBundle:LessonsTarget')->find($request->get('id'));
-
-
-
-                if ($action == 'delete') {
-
-                    $em->remove($target);
-
-                    $em->flush();
-
-
-
-                    $returns['status'] = true;
-
-
-
-                    return new JsonResponse($returns);
-
-                }
-
-            }
-
-
-
-
-
-            if (isset($target) && $target) {
-
-                $target->setTitle($request->get('title'));
-
-                $em->persist($target);
-
+            if ($action == 'delete') {
+                $em->remove($target);
                 $em->flush();
 
+                $returns['status'] = true;
 
-
-                $returns['target'] = $target;
-
+                return new JsonResponse($returns);
             }
-
         }
+        if (isset($target) && $target) {
+            $target->setTitle($request->get('title'));
 
+            $em->persist($target);
 
+            $em->flush();
 
-        return $this->render('AppAdminBundle:Settings/LessonsTargets:row.html.twig', $returns);
-
+            $returns['target'] = $target;
+        }
     }
+
+    return $this->render('AppAdminBundle:Settings/LessonsTargets:row.html.twig', $returns);
+}
